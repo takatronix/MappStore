0221318c65f212027e34cf561d0efee38451a6e8
takatronix <takatronix@gmail.com>
2018-07-16 10:46:38 +0900

    modified typing app

--- src/main/java/red/man10/mappstore/apps/TypingApp.java
+++ src/main/java/red/man10/mappstore/apps/TypingApp.java
@@ -27,8 +27,8 @@ public class TypingApp extends MappApp implements Listener{
         boolean title = true;
         double gameTime;
         boolean TypingCheck;
-        int TypingCount;
-        double wardCount;
+        double TypingCount;
+        double wordCount;
         boolean ranking;
         int random;
         double score;
@@ -38,6 +38,7 @@ public class TypingApp extends MappApp implements Listener{
         boolean getMessage;
         boolean saveRanking;
         int awayTime;
+        double missCount;
 
     }
 
@@ -60,7 +61,7 @@ public class TypingApp extends MappApp implements Listener{
     static String chatMessage = "§8§l[TypingApp]";
     static ArrayList<String> enterMessage = new ArrayList<String>();
 
-    static int TypingTimes;
+    static double TypingTimes;//回数
 
     static FileConfiguration config = MappRenderer.getAppConfig(appName);
 
@@ -71,7 +72,7 @@ public class TypingApp extends MappApp implements Listener{
 
             //      Configファイルに保存する
 
-            TypingTimes = config.getInt("TypingTimes",0);
+            TypingTimes = config.getDouble("TypingTimes",0);
 
             if (config.getInt("TypingTimes") == 0){
                 config.set("TypingTimes",5);
@@ -79,11 +80,13 @@ public class TypingApp extends MappApp implements Listener{
                 MappRenderer.saveAppConfig(appName,config);
             }
             if (config.getString("ranking.player.1") == null||
-                    config.getString("ranking.player.5") == null){
+                    config.getString("ranking.player.3") == null){
 
-                for (int i = 1;i<=5;i++) {
+                for (int i = 1;i<=3;i++) {
                     config.set("ranking.player."+i,"none");
                     config.set("ranking.score."+i,0.0);
+                    config.set("ranking.time."+i,0.0);
+                    config.set("ranking.accuracy."+i,0.0);
 
                 }
             }
@@ -110,10 +113,12 @@ public class TypingApp extends MappApp implements Listener{
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if (!wards.exists()){
-                System.out.println("Cord1");
+
+            if (enterMessage.size() <=1){
+                System.out.println("error1");
+                return true;
             }
-            System.out.println("config,words");
+
             return true;  //  true -> update map / trueでマップに画像が転送されます
         });
 
@@ -126,21 +131,24 @@ public class TypingApp extends MappApp implements Listener{
             if (data.title) {
                 MappDraw.drawImage(g,"typing_background",0,0,128,128);
 
+                g.setFont(new Font("SansSerif", Font.PLAIN, 18));
+
+                MappDraw.drawOutlineString(g,"TypingGame", Color.WHITE,Color.BLACK,12, 20);
                 g.setFont(new Font("SansSerif", Font.PLAIN, 10));
 
-                MappDraw.drawOutlineString(g,"TypingGame", Color.WHITE,Color.BLACK,30, 10);
                 MappDraw.drawOutlineString(g,"Push the button to start!",  Color.WHITE,Color.BLACK,10, 50);
                 MappDraw.drawOutlineString(g,"ボタンを押して",  Color.WHITE,Color.BLACK,10, 60);
                 MappDraw.drawOutlineString(g,"ゲームスタート！",  Color.WHITE,Color.BLACK,10, 70);
                 g.setFont(new Font("SansSerif", Font.PLAIN, 9));
                 MappDraw.drawOutlineString(g,"一定時間放置すると",  Color.WHITE,Color.BLACK,10, 80);
                 MappDraw.drawOutlineString(g,"自動でタイトルに戻ります",  Color.WHITE,Color.BLACK,10, 89);
+                MappDraw.drawOutlineString(g,"Touch title to ranking!",  Color.WHITE,Color.BLACK,10, 100);
 
             }else {
                     gameMain(data.gamePlayer,mapId,g);
                 }
-            if (data.ranking){
-                loadRanking(g);
+            if (data.ranking&&data.title){
+                loadRanking(g,mapId);
             }
 
             return true;  //  true -> update map / trueでマップに画像が転送されます
@@ -164,6 +172,9 @@ public class TypingApp extends MappApp implements Listener{
                     data.TypingCheck = true;
                     data.gameTime = 0;
                     data.saveRanking = true;
+                    data.awayTime = 0;
+                    data.missCount = TypingTimes;
+
 
                 } else {
                     finish(mapId);
@@ -182,8 +193,8 @@ public class TypingApp extends MappApp implements Listener{
             TypingData data = loadData(mapId);
             if (data.title){
                 data.ranking = !data.ranking;
+                data.awayTime = 0;
                 saveData(mapId,data);
-
             }
 
             return true;
@@ -194,9 +205,18 @@ public class TypingApp extends MappApp implements Listener{
         TypingData data = loadData(mapId);
         Player p = chatEvent.getPlayer();
         if (data.getMessage && p == data.gamePlayer) {
+            if (data.awayTime <=80){
+                p.sendMessage(chatMessage+"§4まだゲームは始まっていません！");
+                chatEvent.setFormat("");
+                chatEvent.setCancelled(true);
+
+                return true;
+            }
             data.playerMessage = chatEvent.getMessage();
+            chatEvent.setFormat("");
             chatEvent.setCancelled(true);
             data.sendChat = true;
+            System.out.println(p.getName()+data.playerMessage);
             saveData(mapId, data);
         }
             return true;
@@ -204,21 +224,27 @@ public class TypingApp extends MappApp implements Listener{
 
     }
 
-    private static void saveRanking(int mapId,double score){
+    private static void saveRanking(int mapId,double score,double time,double accuracy){
         TypingData data = loadData(mapId);
         data.saveRanking = false;
 
-        for (int i = 1;i<=5;i++){
+        for (int i = 1;i<=3;i++){
             if (data.score >=config.getDouble("ranking.score."+i)){
-                if (i !=5){
+                if (i !=3){
                     String p = config.getString("ranking.player."+i);
-                    Double s = config.getDouble("ranking.score."+i);
+                    double s = config.getDouble("ranking.score."+i);
+                    double t = config.getDouble("ranking.time."+i);
+                    double ac = config.getDouble("ranking.accuracy."+i);
                     int i_2 = i+1;
                     config.set("ranking.player."+i_2,p);
                     config.set("ranking.score."+i_2,s);
+                    config.set("ranking.time."+i_2,t);
+                    config.set("ranking.accuracy."+i_2,ac);
                 }
                 config.set("ranking.score."+i,score);
                 config.set("ranking.player."+i,data.gamePlayer.getName());
+                config.set("ranking.time."+i,time);
+                config.set("ranking.accuracy."+i,accuracy);
 
                 MappRenderer.saveAppConfig(appName,config);
 
@@ -248,61 +274,65 @@ public class TypingApp extends MappApp implements Listener{
 
             g.setFont(new Font("", Font.PLAIN, 10));
 
-            String typing = format("%.1f",data.wardCount/data.gameTime);
-            String time = format("%.1f",data.gameTime);
-            MappDraw.drawOutlineString(g,"タイム："+time,Color.WHITE,Color.YELLOW,10,30);
+            String typing = format("%.1f",data.wordCount/data.gameTime);//秒間タイピング数
+            String time = format("%.1f",data.gameTime);//かかった時間
+            MappDraw.drawOutlineString(g,"タイム："+time+"秒",Color.WHITE,Color.YELLOW,10,30);
             MappDraw.drawOutlineString(g,"秒間"+typing+"typing",Color.WHITE,Color.YELLOW,10,40);
-            data.score = (data.wardCount/data.gameTime)+TypingTimes*100-data.gameTime;
+            data.score = (data.wordCount/data.gameTime)*(data.missCount/TypingTimes)*100.0;
             String s = format("%.1f",data.score);
 
-            MappDraw.drawOutlineString(g, format("スコア："+"%.1f",data.score),Color.WHITE,Color.YELLOW,10,50);
+            MappDraw.drawOutlineString(g, s,Color.WHITE,Color.YELLOW,10,50);
             MappDraw.drawOutlineString(g, "Player:"+player.getName(),Color.WHITE,Color.YELLOW,10,60);
 
             MappDraw.drawOutlineString(g,"ボタンを押すまで",Color.WHITE,Color.BLACK,5,80);
             MappDraw.drawOutlineString(g,"チャットを打てません",Color.WHITE,Color.BLACK,5,90);
+
+
             if (data.saveRanking){
+                data.awayTime = 100;
                 player.sendMessage(chatMessage+"§2終了！！");
                 player.sendMessage(chatMessage+"§b§lPlayerName:"+player.getName()+",time:"+time+",typing"+typing+"typing/s"+",score:"+s);
-                Double score = Double.parseDouble(s);
-
-                saveRanking(mapId,score);
+                double score = Double.parseDouble(s);//スコア
+                double times = Double.parseDouble(time);//かかった時間
+                double accuracy = (data.missCount/TypingTimes)*100;//精度
+                saveRanking(mapId,score,times,accuracy);
             }
 
             saveData(mapId,data);
 
             return;
         }
-        data.gameTime +=0.1;//error
-        if (enterMessage.size() <=1){
-            System.out.println("error1");
-            return;
-        }
-
+        data.gameTime +=0.1;
         if (data.TypingCheck) {//タイピング成功orスタート時
+            data.awayTime = 100;
+
             MappDraw.drawImage(g,"typing_background",0,0,128,128);
-
-
-            int checkNum = data.random;
+            int ran = data.random;
             data.random  = new java.util.Random().nextInt(enterMessage.size() -1);
-            if (checkNum==data.random){
-                data.random  = new java.util.Random().nextInt(enterMessage.size() -1);
-            }
+            if (ran == data.random)data.random = new java.util.Random().nextInt(5);
             g.setFont(new Font("SansSerif", Font.BOLD, 10));
             MappDraw.drawOutlineString(g,"Player:"+player.getName(),Color.WHITE,Color.BLACK,10,90);
             MappDraw.drawOutlineString(g,enterMessage.get(data.random),Color.WHITE,Color.BLACK,10,50);
             data.TypingCheck = false;
+            saveData(mapId,data);
+            return;
 
         }
         if (data.sendChat) {//文字のチェック、失敗したときの処理
+            data.awayTime = 100;
             if (data.playerMessage.equals(enterMessage.get(data.random))) {//判定、カウント、合ってなかったらスルー
                 data.TypingCheck = true;
                 data.TypingCount++;
-                data.wardCount += data.playerMessage.length();
+                data.wordCount += data.playerMessage.length();
 
                 player.sendMessage(chatMessage + "§2入力成功！");
 
             }if (!data.playerMessage.equals(enterMessage.get(data.random))){
                 player.sendMessage(chatMessage + "§4打ち間違えています");
+                data.missCount--;
+                if (data.missCount==0){
+                    data.missCount = 0;
+                }
             }
             data.sendChat = false;
             data.playerMessage = "";
@@ -311,21 +341,34 @@ public class TypingApp extends MappApp implements Listener{
         saveData(mapId,data);
 
     }
-    private static void loadRanking(Graphics2D g) {
+    private static void loadRanking(Graphics2D g,int mapId) {
         MappDraw.drawImage(g,"typing_background",0,0,128,128);
 
-        String th;
-        Color ranking_color;
+        TypingData data = loadData(mapId);
+
+        data.awayTime ++;
+
+        if (data.awayTime == config.getInt("AwayTime")){
+            data.title = true;
+            data.ranking = false;
+            saveData(mapId,data);
+            return;
+        }
+        saveData(mapId,data);
+
+
+        String th = "th";
+        Color ranking_color = Color.YELLOW;
 
         g.setColor(Color.black);
-        g.setFont(new Font( "SansSerif", Font.PLAIN,12));
-        MappDraw.drawOutlineString(g,"<RANKING>",Color.white,Color.yellow,15,18);
-        g.setFont(new Font( "SansSerif", Font.PLAIN,10));
+        g.setFont(new Font( "SansSerif", Font.PLAIN,16));
+        MappDraw.drawOutlineString(g,"<RANKING>",Color.white,Color.yellow,18,18);
+//        g.setFont(new Font( "SansSerif", Font.PLAIN,10));
         MappDraw.drawOutlineString(g,"Player/Score",Color.white,Color.yellow,18,30);
         g.setFont(new Font( "SansSerif", Font.PLAIN,12));
 
 
-        for (int i = 1; i<=5; i++){
+        for (int i = 1; i<=3; i++){
             switch (i){
                 case 1:
                     th="st";
@@ -336,12 +379,11 @@ public class TypingApp extends MappApp implements Listener{
                 case 3:
                     th="rd";
                     ranking_color= Color.RED;break;
-                default:
-                    ranking_color= Color.BLACK;
-                    th="th";break;
             }
             MappDraw.drawOutlineString(g,i+th+"/"+config.getString("ranking.player."+i)+
-                    "/"+config.getDouble("ranking.score."+i),Color.WHITE,ranking_color,2,i*13+32);
+                    "/"+config.getDouble("ranking.score."+i),Color.WHITE,ranking_color,2,i*26+24);
+            MappDraw.drawOutlineString(g,"Times"+config.getDouble("ranking.time."+i)+"/Acc"+
+                    config.getDouble("ranking.accuracy."+i)+"%",Color.white,ranking_color,8,i*26+37);
 
         }
     }
@@ -352,10 +394,9 @@ public class TypingApp extends MappApp implements Listener{
         data.getMessage = false;
         data.title = true;
         data.TypingCount = 0;
-        data.wardCount = 0;
+        data.wordCount = 0;
         data.score = 0;
         data.gamePlayer = null;
-        data.awayTime = 0;
         saveData(mapId,data);
 
     }
